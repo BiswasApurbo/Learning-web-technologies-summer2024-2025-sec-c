c0610e6b5626c69277d4fab199cd977e5929948b
BiswasApurbo <apurbobiswas32@gmail.com>
2025-08-18 15:38:09 +0600

    php added

---
+++ Php/test.php
@@ -0,0 +1,49 @@
+<?php
+
+// $name = 'apurbo';
+// $age = 12;
+// $cgpa = 3.5;
+// $std = array(1,'alamin',3.5);
+// $std[2];
+// $std = [1, 'alamin', 3.5];
+// $stds = [
+//     [1, 'alamin', 3.4],
+//     [2, 'xyz', 3.2],
+//     [3,'abc', 3.3]
+// ];
+// $stds[2][1];
+// $std = ['id'=>1, 'name'=>'alamin', 'cgpa'=>3.4];
+// $std['id'];
+
+// $stds = [
+//     's1' => ['id'=>1, 'name'=>'alamin', 'cgpa'=>3.4],
+//     's2' => ['id'=>2, 'name'=>'xyz', 'cgpa'=>3.2],
+//     's3' => ['id'=>3, 'name'=>'abc', 'cgpa'=>3.3]
+// ];
+// $stds['s2']['cgpa']
+
+// fuction abc($a = 0, $b = 10){
+//     return $a+$b;
+// }
+// $c = abc();
+
+// if(10 === "10"){
+
+// }else{
+
+// }
+
+// for($i=0; $i<10; i++){
+//     $std[i]
+// }
+
+// foreach( $std as $v ){
+//     $v;
+// }
+
+for($i=0; $i<10; $i++){
+    echo "Apurbo <br>";
+}
+echo "Hello from PHP </h1>";
+print('Hello from PHP print func....');
+?>
